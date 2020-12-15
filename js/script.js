1c64a4f950119169aa6c0346d30728b6b358da29
Davide <davide.vignozzi@gmail.com>
2020-12-15 14:47:21 +0100

    Fine email

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
 //EMAIL
-
 // chiedo l'email
+var email = prompt('inserisci l\'email')
 
 /* controllo hce l'email sia nell'array
 se c'è vai avanti
 altrimenti allert con errore
 */
+var mailLogged = ['boolean@boolean.it', 'davide@boolean.it', 'qualcuno@boolean.it']
+
+if (email === mailLogged[0] || email === mailLogged[1] || email === mailLogged[2]) {
+  console.log(email + ' ha effettuato l\'accesso');
+  var logged = document.getElementById('check-email')
+  logged.innerText = 'Hai effettuato l\'accesso'
+} else {
+  alert('Errore')
+}
+
+// DADI
